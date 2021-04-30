7d856a95475e2e75fa4e905c0f51f40330b5a268
Ellis Hogan <ellishogan@Elliss-Mac-mini.local>
2021-04-30 11:01:16 -0400

    update

--- src/pages/components/navBar.js
+++ src/pages/components/navBar.js
@@ -6,9 +6,6 @@ let NavBar = (props)=> {
         <nav>
             <div className="d-flex flex-row justify-content-end align-items-center nav-bar">
                     <a href="/">Home</a> |
-                    <a href="/about">About Us</a> |
-                    <a href="/projects-and-portfolio">Portfolio</a> |
-                    <a href="/contact-us">Contact Us</a> |
                     <a href="/get-quote">Get Quote</a> |
                     <a href="/contact-us">Contact Us</a>
             </div>
