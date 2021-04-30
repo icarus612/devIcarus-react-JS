c83aac7adb60d5a444cc1bc397902314b4d145dd
Ellis Hogan <ellishogan@Elliss-Mac-mini.local>
2021-04-30 10:42:13 -0400

    update

--- src/pages/components/navBar.js
+++ src/pages/components/navBar.js
@@ -2,25 +2,15 @@ import React from 'react';
 import IndexRoutes from '../../routes.js';
 
 let NavBar = (props)=> {
-    let toggle = ()=> {
-        let linksEl = document.querySelector('.narrowLinks');
-        if (linksEl.style.display === 'block') {
-                  linksEl.style.display = 'none';
-              } else {
-                  linksEl.style.display = 'block';
-              }
-    }
     return (
         <nav>
-            <div className="d-flex flex-row justify-content-end align-items-center">
-                <div className="d-flex flex-row justify-content-around align-items-center col-12 col-md-6 col-lg-4">
-                    <a href="/">Home</a>
-                    <a href="/about">About Us</a>
-                    <a href="/projects-and-portfolio">Portfolio</a>
+            <div className="d-flex flex-row justify-content-end align-items-center nav-bar">
+                    <a href="/">Home</a> |
+                    <a href="/about">About Us</a> |
+                    <a href="/projects-and-portfolio">Portfolio</a> |
+                    <a href="/contact-us">Contact Us</a> |
+                    <a href="/get-quote">Get Quote</a> |
                     <a href="/contact-us">Contact Us</a>
-                    <a href="/get-quote">Get Quote</a>
-                    <a href="/contact-us">Contact Us</a>
-                </div>
             </div>
         </nav>
        
